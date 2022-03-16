acab310e9c9b9f68611b41de9966554f4b63636b
yuanwenlai_a <yuanwenlai_a@aspirecn.com>
2022-03-16 23:31:52 +0800

    add commmander

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-// console.log("Hello TS")
-let str:string = 'Hello TS'
+import { Command } from 'commander';
+const program = new Command();
+program
+    .option('-V, --vsersion', 'output version number')
+    .option('-c, --city', 'add city name')
 
-
-
+program.parse(process.argv);
+const options = program.opts();
+    // console.log(233,options.city)
 
